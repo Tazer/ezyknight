4c209cd556b8a3ead225b9d7ca7b2995551ad32a
Patrik <rambo3swe@gmail.com>
2012-12-10 19:02:37 +0100

    inital movement implemented

--- src/app/ezyKnight/Hubs/GameHub.cs
+++ src/app/ezyKnight/Hubs/GameHub.cs
@@ -9,7 +9,9 @@ namespace ezyKnight.Hubs
 {
     public class Player
     {
+        public string Id { get; set; }
         public string Name { get; set; }
+        public string Color { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
     }
@@ -21,15 +23,16 @@ namespace ezyKnight.Hubs
         public static IDictionary<string, Player> Players = new Dictionary<string, Player>();
         private string Motd = "Welcome to EzyKnight";
 
-        public Task Join(string name)
+        public Task Join(string name, string color)
         {
             if (Players.Any(x => x.Value.Name == name))
             {
                 return Clients.Caller.addChatMessage("Player with that name already exsists");
             }
 
-            Players.Add(Context.ConnectionId, new Player() { Name = name });
-            return Groups.Add(Context.ConnectionId, "Players");
+            Players.Add(Context.ConnectionId, new Player() { Id = Context.ConnectionId, Name = name, Color = color });
+            Groups.Add(Context.ConnectionId, "Players");
+            return Clients.Caller.joined(Players.ToArray());
         }
 
         public Task Send(string message)
@@ -37,9 +40,22 @@ namespace ezyKnight.Hubs
             return Clients.OthersInGroup("Players").addChatMessage(Players[Context.ConnectionId].Name + " -> " + message);
         }
 
+        public Task Move(int x, int y)
+        {
+            if (!Players.ContainsKey(Context.ConnectionId))
+            {
+                return Clients.Caller.addChatMessage("Player hasnt joined");
+            }
+
+            var player = Players[Context.ConnectionId];
+            player.X = x;
+            player.Y = y;
+            return Clients.Group("Players").moved(player);
+        }
+
         public override Task OnConnected()
         {
-            return Clients.Group("Players").joined(Context.ConnectionId);
+            return Clients.Group("Players").newconnection(Context.ConnectionId);
         }
     }
 }
